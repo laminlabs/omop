499f157a53eb13768a8ac396bfab7eb46ed189bd
Sunny Sun <38218185+sunnyosun@users.noreply.github.com>
2024-09-16 13:40:59 +0200

    💚 Fix

--- tests/test_notebooks.py
+++ tests/test_notebooks.py
@@ -6,5 +6,5 @@ import nbproject_test as test
 def test_notebooks():
     docs_folder = Path(__file__).parents[1] / "docs/"
     for check_folder in docs_folder.glob("./**"):
-        if check_folder.name != "notes":
+        if check_folder.name != "inspectdb":
             test.execute_notebooks(check_folder, write=True)
