21c28a5d83b73945df16ec1e8e69f5c2adfdb5d6
Sunny Sun <38218185+sunnyosun@users.noreply.github.com>
2024-09-16 13:06:57 +0200

    ♻️ Fix CI

--- docs/index.md
+++ docs/index.md
@@ -8,6 +8,7 @@
 :hidden:
 
 guide
+notes
 reference
 changelog
 ```
--- tests/test_notebooks.py
+++ tests/test_notebooks.py
@@ -6,4 +6,5 @@ import nbproject_test as test
 def test_notebooks():
     docs_folder = Path(__file__).parents[1] / "docs/"
     for check_folder in docs_folder.glob("./**"):
-        test.execute_notebooks(check_folder, write=True)
+        if check_folder.name != "notes":
+            test.execute_notebooks(check_folder, write=True)
